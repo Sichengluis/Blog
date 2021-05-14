a82586d4bafae69fffb9a345cac4599086eab7c7
Sichengluis <sichengluis@gmail.com>
2021-05-14 19:45:14 +0800

    添加分类页面中分类标签的收起和显示更多功能，修改pageSizeCofig文件名

--- src/main/java/com/scluis/config/paraConfig.java
+++ src/main/java/com/scluis/config/paraConfig.java
@@ -4,12 +4,20 @@ package com.scluis.config;
  * Created by Sichengluis on 2021/2/14 17:24
  */
 public class pageSizeConfig {
+    //右侧热门分类显示数量
     public static final int topTypesPageSize=3;
+    //右侧标签显示数量
     public static final int topTagsPageSize=5;
+    //博主推荐显示数量
     public static final int recentRecommendedBlogsPageSize=3;
+    //热门文章显示数量
     public static final int hotBlogsPageSize=3;
+    //最新文章显示数量
     public static final int newBlogsPageSize=3;
+    //管理员博客页面一页显示数量
     public static final int adminBlogSize=12;
+    //管理员分类页面一页显示数量
     public static final int adminTypeSize=12;
+    //管理员标签页面一页显示数量
     public static final int adminTagSize=12;
 }
